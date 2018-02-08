24679932a5a4c04352c757aad217cf859dd43a99
Edward Pritchard <epr@atolcd.com>
2018-02-08 11:43:06 +0100

    Ajout d'un SQL CatchUp
    Change-Id: Ie6507520733c950af2ffcf63b2de06569f68c9e1

---
+++ app/Config/sql/patches/3.x/patch-3.2.x-000-catchup.sql
@@ -0,0 +1,4 @@
+\i '/var/www/webrsa/app/Config/sql/patches/3.x/patch-3.2.0.sql'
+\i '/var/www/webrsa/app/Config/sql/patches/3.x/patch-3.2.3.sql'
+\i '/var/www/webrsa/app/Config/sql/patches/3.x/patch-3.2.4.sql'
+\i '/var/www/webrsa/app/Config/sql/patches/3.x/patch-3.2.5.sql'
