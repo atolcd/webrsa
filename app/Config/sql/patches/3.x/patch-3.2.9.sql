709153c8d051c1dbfe0dc2eef08e6e80cfc8c5a7
epr <epr@atolcd.com>
2018-10-30 16:59:15 +0100

    Correction et merge des SQL 3.2.9
    - Correction d'un virgule dans les lignes créances
    - Concaténation du fichier des relances SMS
    
    Change-Id: I0f3721be934dee2c5b7fc337e12a9c5100fc872c

--- app/Config/sql/patches/3.x/patch-3.2.9.sql
+++ app/Config/sql/patches/3.x/patch-3.2.9.sql
@@ -11,6 +11,9 @@ SET default_with_oids = false;
 BEGIN;
 -- *****************************************************************************
 
+-- /****************************************************************
+-- Modification des créances
+
 ALTER TABLE creances ADD COLUMN moismoucompta date;
 ALTER TABLE creances ADD COLUMN orgcre character(3) NOT NULL DEFAULT 'FLU'::character varying;
 ALTER TABLE creances ADD COLUMN haspiecejointe character varying(1) NOT NULL DEFAULT '0'::character varying;
@@ -18,12 +21,9 @@ ALTER TABLE creances ADD COLUMN hastitrecreancier integer NOT NULL DEFAULT 0;
 
  UPDATE creances SET  orgcre='FLU' WHERE orgcre NOT LIKE 'MAN' AND orgcre NOT LIKE 'FLU' AND orgcre IS NULL;
 
+-- /****************************************************************
+-- Creation de la table des titres emmis
 
-/*
-*
-* Creation de la table des titres emmis
-*
-*/
 CREATE TABLE titrescreanciers (
   id serial NOT NULL,
   creance_id integer NOT NULL,
@@ -41,17 +41,13 @@ CREATE TABLE titrescreanciers (
   bic character varying(12),
   titulairecompte character varying(80),
   numtel character varying(14),
-  haspiecejointe character varying(1) DEFAULT '0'::character varying,
+  haspiecejointe character varying(1) DEFAULT '0'::character varying
 );
 
 DROP INDEX IF EXISTS titrescreanciers_id_idx;
 CREATE UNIQUE INDEX titrescreanciers_id_idx ON titrescreanciers( id );
 
-/*
-*
-* Etats des titres créanciers
-*
-*/
+-- Etats des titres créanciers
 DROP TABLE IF EXISTS etatstitrescreanciers CASCADE;
 CREATE TABLE etatstitrescreanciers(
   id			SERIAL NOT NULL PRIMARY KEY,
@@ -65,7 +61,7 @@ COMMENT ON TABLE etatstitrescreanciers IS 'Liste des etats des titres creanciers
 DROP INDEX IF EXISTS etatstitrescreanciers_name_idx;
 CREATE UNIQUE INDEX etatstitrescreanciers_name_idx ON etatstitrescreanciers( name );
 
-/* Insertion de 'CRE', 'VAL', 'SEN', 'PAY','SUP' */
+-- Insertion de 'CRE', 'VAL', 'SEN', 'PAY','SUP'
 INSERT INTO etatstitrescreanciers
 	( name, actif, created, modified )
 VALUES
@@ -76,6 +72,7 @@ VALUES
 	('Imprimer', 1, NOW(), NOW() )
 ;
 
+-- table de lien 
 DROP TABLE IF EXISTS etatstitrescreanciers_titrescreanciers CASCADE;
 CREATE TABLE etatstitrescreanciers_titrescreanciers (
   id                 			SERIAL NOT NULL PRIMARY KEY,
@@ -91,11 +88,7 @@ CREATE INDEX etatstitrescreanciers_titrescreanciers_etattitrecreancier_id_idx ON
 DROP INDEX IF EXISTS etatstitrescreanciers_titrescreanciers_titrecreancier_id_idx;
 CREATE INDEX etatstitrescreanciers_titrescreanciers_titrecreancier_id_idx ON etatstitrescreanciers_titrescreanciers(titrecreancier_id);
 
-/*
-*
-* Type des titres creanciers
-*
-*/
+-- Type des titres creanciers
 DROP TABLE IF EXISTS typestitrescreanciers CASCADE;
 CREATE TABLE typestitrescreanciers(
   id			SERIAL NOT NULL PRIMARY KEY,
@@ -109,7 +102,7 @@ COMMENT ON TABLE typestitrescreanciers IS 'Liste des types des titres creanciers
 DROP INDEX IF EXISTS typestitrescreanciers_name_idx;
 CREATE UNIQUE INDEX typestitrescreanciers_name_idx ON typestitrescreanciers( name );
 
-/* Insertion des valeur de départ */
+-- Insertion des valeur de départ
 INSERT INTO typestitrescreanciers
 	(name,actif,created,modified)
 VALUES
@@ -120,6 +113,7 @@ VALUES
 	('Creance complet - Emis au nom de MR', 1, NOW(), NOW() )
 ;
 
+-- table de lien 
 DROP TABLE IF EXISTS typestitrescreanciers_titrescreanciers CASCADE;
 CREATE TABLE typestitrescreanciers_titrescreanciers (
   id                 			SERIAL NOT NULL PRIMARY KEY,
@@ -135,9 +129,9 @@ CREATE INDEX typestitrescreanciers_titrescreanciers_typetitrecreancier_id_idx ON
 DROP INDEX IF EXISTS typestitrescreanciers_titrescreanciers_titrecreancier_id_idx;
 CREATE INDEX typestitrescreanciers_titrescreanciers_titrecreancier_id_idx ON typestitrescreanciers_titrescreanciers(titrecreancier_id);
 
-/*
-* Creation de la table des rapport crée par les talends d'intégration des flux CNAF
-*/
+-- *********************************************************************************
+-- Creation de la table des rapport crée par les talends d'intégration des flux CNAF
+
 CREATE TABLE administration.rapportstalendscreances (
   id serial NOT NULL,
   flux character varying(15),
@@ -156,9 +150,7 @@ CREATE TABLE administration.rapportstalendscreances (
   message character varying(1000)
 );
 
-/*
-* Creation de la table des personnes rejetées par les talends d'intégration des flux CNAF
-*/
+-- Creation de la table des personnes rejetées par les talends d'intégration des flux CNAF
 CREATE TABLE administration.rejetstalendscreances (
 	id 					serial NOT NULL,
 	fusion 				BOOLEAN DEFAULT FALSE,
@@ -219,6 +211,58 @@ CREATE TABLE administration.rejetstalendscreances (
 	liblig6adr 			VARCHAR(38) DEFAULT NULL,
 	liblig7adr 			VARCHAR(38) DEFAULT NULL
 );
+
+
+
+-- *****************************************************************************
+-- Relances
+
+ALTER TABLE "structuresreferentes" ADD COLUMN "lib_struc_mini" CHARACTER varying(100);
+
+CREATE TABLE relances
+(
+  id serial NOT NULL,
+  relancesupport character varying(20) NOT NULL,
+  relancetype character varying(20) NOT NULL,
+  relancemode character varying(30) NOT NULL,
+  nombredejour integer,
+  contenu text,
+  actif smallint NOT NULL DEFAULT 0,
+  created timestamp without time zone,
+  modified timestamp without time zone,
+  CONSTRAINT relances_pkey PRIMARY KEY (id),
+  CONSTRAINT relances_relancesupport_in_list_chk CHECK (cakephp_validate_in_list(relancesupport::text, ARRAY['SMS'::text, 'EMAIL'::text])),
+  CONSTRAINT relances_relancetype_in_list_chk CHECK (cakephp_validate_in_list(relancetype::text, ARRAY['RDV'::text, 'EP'::text])),
+  CONSTRAINT relances_relancemode_in_list_chk CHECK (cakephp_validate_in_list(relancemode::text, ARRAY['ORANGE_CONTACT_EVERYONE'::text, 'EMAIL'::text]))
+)
+WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE relanceslogs
+(
+  id serial NOT NULL,
+  personne_id integer,
+  nom_complet character varying(255),
+  numport character varying(255),
+  email character varying(255),
+  daterdv date,
+  heurerdv time without time zone,
+  lieurdv character varying(255),
+  relancetype character varying(20),
+  nombredejour integer,
+  contenu text,
+  statut character varying(20),
+  support character varying(50),
+  mode character varying(50),
+  created timestamp without time zone,
+  modified timestamp without time zone,
+  CONSTRAINT relanceslogs_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+
 
 -- *****************************************************************************
 COMMIT;
