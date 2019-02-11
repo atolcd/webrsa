dd9efe0f43ec485d2b1678a9decdb65f7ba505e0
Guillaume POITTEVIN <gpo@atolcd.com>
2019-02-11 15:58:09 +0100

    Cleanup all cakephp files & keep project files
    Change-Id: I77e8210c409162adbe43555680213855c51fd3a2

--- app/Config/sql/patches/1.x/patch-version1.0.7-20090820-15h19.sql
+++
@@ -1,5 +0,0 @@
---------------- Ajout du 20/08/2009 à 15h20 ------------------
-ALTER TABLE structuresreferentes ALTER COLUMN num_voie TYPE VARCHAR(15);
-ALTER TABLE structuresreferentes ALTER COLUMN nom_voie TYPE VARCHAR(50);
-
-ALTER TABLE servicesinstructeurs ALTER COLUMN num_rue TYPE VARCHAR(15);
--- app/Config/sql/tests/test.pourmichel.20090708-17h21.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM structuresreferentes
-    WHERE typeorient_id IN ( SELECT id FROM typesorients WHERE typesorients.parentid IS NOT NULL );
-
-DELETE FROM typesorients WHERE typesorients.parentid IS NOT NULL;
