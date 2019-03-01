138df9608268b6523a532135083eca0bf50ef505
Pierre Lavigne <pla@atolcd.com>
2019-03-01 11:33:20 +0100

    Modification webrsa.sh
    Change-Id: I8fcfbba0bb1e701fb30c8ecd696830d743fa2a80

--- project/app/webrsa.sh
+++ project/app/webrsa.sh
@@ -2,15 +2,16 @@
 
 ME="$0"
 APP_DIR="`dirname "$ME"`"
-WORK_DIR="$PWD"
-RELEASES_DIR="$WORK_DIR/releases"
-ChangeLog="ChangeLog.txt"
-ASNV="ssh://gerrit:29418/cd93/webrsa"
-YUICOMPRESSOR="$HOME/bin/yuicompressor.jar"
-echo $APP_DIR
+MAIL="webrsa@atolcd.com"
+
+echo ""
+echo "     ----------------------------------------------------------------------"
+echo "     $APP_DIR"
+echo "     ----------------------------------------------------------------------"
+echo ""
+
 
 # ------------------------------------------------------------------------------
-
 function __clearDir() {
 	local dir="$1"
 
@@ -23,8 +24,8 @@ function __clearDir() {
 	fi
 }
 
-# ------------------------------------------------------------------------------
 
+# ------------------------------------------------------------------------------
 function __clear() {
 	local dir="$1"
 
@@ -38,134 +39,51 @@ function __clear() {
 	rm -f $dir/tmp/logs/*.log
 	rm -f $dir/tmp/logs/*_odt
 	rm -f $dir/tmp/logs/*.csv
-
-# 	rm "$dir/webroot/js/webrsa.js">> /dev/null 2>&1
-# 	rm "$dir/webroot/css/webrsa.css" >> /dev/null 2>&1
 }
 
-# ------------------------------------------------------------------------------
-
-function __minify() {
-	if [ ! -e "$YUICOMPRESSOR" ];then
-		echo "Le fichier $YUICOMPRESSOR n'existe pas (pensez à changer le chemin vers le yuicompressor dans ce script)."
-		return 1
-	fi
-
-	JSDIR="$1/webroot/js"
-	CSSDIR="$1/webroot/css"
-
-	# CSS
-	CSSFILE="$CSSDIR/webrsa.tmp.css"
-	echo "@media all {" > "$CSSFILE"
-	cat "$CSSDIR/all.reset.css" "$CSSDIR/all.base.css" "$CSSDIR/bootstrap.custom.css" "$CSSDIR/all.form.css" "$CSSDIR/fileuploader.css" "$CSSDIR/fileuploader.webrsa.css" "$CSSDIR/permissions.css" "$1/Plugin/Configuration/webroot/css/configuration_parser.css" >> "$CSSFILE"
-	echo "}" >> "$CSSFILE"
-
-	echo "@media screen,presentation {" >> "$CSSFILE"
-	cat "$CSSDIR/menu.css" "$CSSDIR/popup.css" "$CSSDIR/screen.generic.css" "$CSSDIR/screen.search.css" >> "$CSSFILE"
-	echo "}" >> "$CSSFILE"
-
-	echo "@media print {" >> "$CSSFILE"
-	cat "$CSSDIR/print.generic.css" >> "$CSSFILE"
-	echo "}" >> "$CSSFILE"
-
-	java -jar "$YUICOMPRESSOR" "$CSSFILE" -o "$CSSDIR/webrsa.css" --charset utf-8
- 	rm "$CSSFILE"
-
-	# Javascript (Prototype)
-	JSFILE="$JSDIR/webrsa.tmp.js"
-
-	cat "$JSDIR/prototype.js" \
-	"$JSDIR/webrsa.extended.prototype.js" \
-	"$JSDIR/prototype.livepipe.js" \
-	"$JSDIR/prototype.tabs.js" \
-	"$JSDIR/tooltip.prototype.js" \
-	"$JSDIR/webrsa.custom.prototype.js" \
-	"$JSDIR/webrsa.common.prototype.js" \
-	"$JSDIR/prototype.event.simulate.js" \
-	"$JSDIR/dependantselect.js" \
-	"$JSDIR/prototype.maskedinput.js" \
-	"$JSDIR/webrsa.validaterules.js" \
-	"$JSDIR/webrsa.validateforms.js" \
-	"$JSDIR/webrsa.additional.js" \
-	"$JSDIR/fileuploader.js" \
-	"$JSDIR/fileuploader.webrsa.js" \
-	"$JSDIR/cake.prototype.js" \
-	"$JSDIR/webrsa.cake.tabbed.paginator.js" \
-	"$JSDIR/prototype.fabtabulous.js" \
-	"$JSDIR/prototype.tablekit.js" \
-	"$1/Plugin/Configuration/webroot/js/prototype.configuration-parser.js" \
-	> "$JSFILE"
-
-	java -jar "$YUICOMPRESSOR" "$JSFILE" -o "$JSDIR/webrsa.js" --charset utf-8 --preserve-semi
-	rm "$JSFILE"
-}
 
 # ------------------------------------------------------------------------------
-
-function __predeploy() {
-	dir="$1"
-	version="$2"
-
-	echo "Minify"
-	bash $dir/webrsa.sh minify
-
-	echo "Test Codes"
-	chmod +x app/webrsa.sh app/Doc/scripts/find_non_utf8.sh
-	bash $dir//Doc/scripts/find_non_utf8.sh
-	chmod +x app/Doc/scripts/find_whitespace.sh
-	bash $dir/Doc/scripts/find_whitespace.sh
-
-}
-
-# ------------------------------------------------------------------------------
-
 function __deploy() {
 	dir="$1"
-	version="$2"
 
-	echo "minify"
-	bash $dir/webrsa.sh minify
+	echo "     Lien symbolique des modèles odt"
 
-	echo "droits et permissions"
+	APP_LINK=$(readlink -f $(dirname $0))
+	APP_DEPARTEMENT=$(readlink -f $(dirname $0)/../..)
+	ln -s $APP_DEPARTEMENT/contrib/modelesodt $APP_LINK/Vendor/modelesodt
+
+	echo "     $APP_DEPARTEMENT/contrib/modelesodt vers $APP_LINK/Vendor/modelesodt"
+	echo "     Pour supprimer le lien symbolique : unlink $APP_LINK/Vendor/modelesodt"
+	echo ""
+
+	echo "     Droits et permissions"
+	echo ""
+
 	sudo chown apache: -R .
 	sudo chmod u+r,g+r,o+r -R app
 	sudo chmod u+w,g+w -R app/tmp
 	chmod +x vendor/cakephp/cakephp/lib/Cake/Console/cake
 
-	echo "cache"
+	echo "     Cache"
+	echo ""
+
 	bash $dir/webrsa.sh clearcache
-
-	echo "test modelesodt"
-	sudo -u apache vendor/cakephp/cakephp/lib/Cake/Console/cake Gedooo.test_modeles_odt "$dir/Vendor/modelesodt"
-
-}
-# ------------------------------------------------------------------------------
-
-function __deployconfig() {
-	dir="$1"
-	version="$2"
-
-	echo "Déplacement Config"
-
-	cd  $dir/webrsa/app/Console/
-	mkdir /etc/webrsa/
-	rm -Rf /etc/webrsa/*
-	mv $dir/Config/* /etc/webrsa/
-	rm -Rf $dir/Config
-	ln -nfs /etc/webrsa/ $dir/Config
-	cd /var/tmp/
-	cd $dir/
-
 }
 
-# ------------------------------------------------------------------------------
 
- function __cleanODT() {
-             cd "./Vendor/modelesodt" && find . -type f -iname "*.odt.default" | while read -r ; do mv "$REPLY" `echo "$REPLY" |sed 's/\.default$//g'` ; done
+# ------------------------------------------------------------------------------
+function __error() {
+	echo ""
+	echo "     ----------------------------------------------------------------------"
+	echo "     La fonctionnalité << $1 >> est inexistante ou obsolète."
+	echo "     Vous pouvez envoyer votre demande à << $MAIL >> en expliquant votre besoin."
+	echo "     ----------------------------------------------------------------------"
+	echo ""
+	echo ""
 }
 
-# ------------------------------------------------------------------------------
 
+# ------------------------------------------------------------------------------
 case $1 in
 	clear)
 		__clear "$APP_DIR"
@@ -179,28 +97,15 @@ case $1 in
 		__clearDir "$APP_DIR/tmp/logs/"
 		exit 0
 	;;
-	minify)
-		__minify "$APP_DIR"
-		exit 0
-	;;
-	__predeploy)
-		__deploy "$APP_DIR" "$2"
-		exit 0
-	;;
 	deploy)
 		__deploy "$APP_DIR" "$2"
 		exit 0
 	;;
-	deployconfig)
-		__deployconfig "$APP_DIR" "$2"
-		exit 0
-	;;
-	cleanODT)
-		__cleanODT "$APP_DIR"
-		exit 0
-	;;
 	*)
-		echo "Usage: $ME {clearcache|clear|clearlogs|minify|deploy|deployconfig|cleanODT}"
+		echo ""
+		echo "     Usage: $ME {clearcache|deploy}"
+		echo ""
+		__error "$1"
 		exit 1
 	;;
 esac
