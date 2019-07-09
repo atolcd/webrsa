17be873565683fe689825b665b6737d527cefec2
Pierre Lavigne <pla@atolcd.com>
2019-07-09 08:34:26 +0200

    Correction de l'ajout des titres de recette
    Correction du contrôle du tableau des titres de recette qui controle
    l'activation du bouton d'ajout .
    
    Issue: #70190
    Change-Id: Ia60ca11bff7983e917f2dd307094223fa2f835f4

--- project/app/Controller/TitrescreanciersController.php
+++ project/app/Controller/TitrescreanciersController.php
@@ -181,15 +181,14 @@
 				)
 			);
 
-			$titresCreanciers[0]['Titrecreancier']['etatDepuis'] = __d('titrecreancier', 'ENUM::ETAT::' . $titresCreanciers[0]['Titrecreancier']['etat']) . ' depuis le ' . date('d/m/Y', strtotime( $titresCreanciers[0]['Titrecreancier']['modified'] ) );
-
-			$titresCreanciers[0]['Titrecreancier']['acommentaire'] = 0;
-			if( !is_null($titresCreanciers[0]['Titrecreancier']['mention']) ) {
-				$titresCreanciers[0]['Titrecreancier']['acommentaire'] = 1;
-			}
-
 			if ( !empty($titresCreanciers) ){
 				$this->set( 'ajoutPossible', false);
+				$titresCreanciers[0]['Titrecreancier']['etatDepuis'] = __d('titrecreancier', 'ENUM::ETAT::' . $titresCreanciers[0]['Titrecreancier']['etat']) . ' depuis le ' . date('d/m/Y', strtotime( $titresCreanciers[0]['Titrecreancier']['modified'] ) );
+
+				$titresCreanciers[0]['Titrecreancier']['acommentaire'] = 0;
+				if( !is_null($titresCreanciers[0]['Titrecreancier']['mention']) ) {
+					$titresCreanciers[0]['Titrecreancier']['acommentaire'] = 1;
+				}
 			}
 
 			// Historique du titre - si suppression
