6618f669f4e6ad45ea32b9075b24ffe3f95d43f8
epr <epr@atolcd.com>
2020-03-16 14:29:47 +0100

    PLAN PAUVRETE - INSCRITS POLE EMPLOI
    Correction du fonctionnement du filtre par date d'intégration.
    Désormais le filtre par date d'intégration s'assure que la personne est bien SDD DOV et avec une inscription PE supérieur à la date de changement de droit.
    
    Issue: #83799
    Change-Id: Ia073f6b537fe3075d28b478747ad73de12d2f095

--- project/app/Model/WebrsaCohortePlanpauvreteorientationsIsemploistock.php
+++ project/app/Model/WebrsaCohortePlanpauvreteorientationsIsemploistock.php
@@ -53,6 +53,41 @@
 			$query = parent::searchConditions($query, $search);
 			$query['conditions'] = $this->conditionsDates( $query['conditions'], $search, 'Historiquedroit.created' );
 
+			if ( $search ['Historiquedroit']['created'] ){
+				//Modification du lien à Historiquedroit
+				foreach ($query['joins'] as $key => $value) {
+					debug( $value ) ;
+					if ( $value['alias'] == 'Historiquedroit' ){
+						$created_from =
+							$search ['Historiquedroit']['created_from']['year'].'-'.
+							$search ['Historiquedroit']['created_from']['month'].'-'.
+							$search ['Historiquedroit']['created_from']['day'];
+						$created_to =
+							$search ['Historiquedroit']['created_to']['year'].'-'.
+							$search ['Historiquedroit']['created_to']['month'].'-'.
+							$search ['Historiquedroit']['created_to']['day'];
+						$query['joins'][$key] =
+						$this->Personne->join('Historiquedroit', array(
+							'type' => 'INNER',
+							'conditions' => array(
+								'Historiquedroit.personne_id = Personne.id AND Personne.id IN(
+									SELECT "Historiquedroit"."personne_id" from historiquesdroits as Historiquedroit 
+									WHERE "Historiquedroit"."personne_id" = "Personne"."id"'
+									.' AND date_trunc(\'day\', "Historiquedroit"."created") BETWEEN \''.$created_from.'\' AND \''.$created_to.'\' '
+									.' ORDER BY "Historiquedroit"."created" DESC LIMIT 1)'
+							)
+						));
+						break;
+					}
+				}
+				// SDD & DOV sur historique
+				$query = $this->sdddovHistorique($query);
+
+				//Gestion de l'inscription Pole emploi
+				$query['conditions'][] = '(date_trunc(\'day\', "Historiqueetatpe"."date") > date_trunc(\'day\', "Historiquedroit"."created")';
+
+			}
+
 			return $query;
 		}
 	}
