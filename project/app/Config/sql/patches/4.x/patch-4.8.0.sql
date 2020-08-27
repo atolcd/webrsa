1c2c132d7f2a959b0fdfc196ffe9bcb2317e5e25
MMa <mma@atolcd.com>
2020-08-27 15:28:14 +0200

    Modification de la variable Module.ModifEtatDossier.etatdos
    Suppression de la ligne SQL liant la variable à la catégorie Modifsetatsdossiers
    
    Issue: #96218
    Change-Id: I7e51b343f0d2bfb9c858b7bb2abc67d584d23b02

--- project/app/Config/sql/patches/4.x/patch-4.8.0.sql
+++ project/app/Config/sql/patches/4.x/patch-4.8.0.sql
@@ -177,8 +177,6 @@ INSERT INTO public.configurations(lib_variable, value_variable, comments_variabl
 		)', current_timestamp, current_timestamp);
 
 UPDATE public.configurations SET configurationscategorie_id = configurationscategories.id FROM configurationscategories WHERE configurationscategories.lib_categorie = 'Modifsetatsdossiers' AND configurations.lib_variable LIKE 'ConfigurableQuery.Modifsetatsdossiers.cohorte_modifetatdos';
-UPDATE public.configurations SET configurationscategorie_id = configurationscategories.id FROM configurationscategories WHERE configurationscategories.lib_categorie = 'Modifsetatsdossiers' AND configurations.lib_variable LIKE 'Module.ModifEtatDossier.etatdos';
-
 -- PPAE
 ALTER TABLE public.sanctionseps58 DROP CONSTRAINT sanctionseps58_origine_in_list_chk;
 ALTER TABLE public.sanctionseps58 ADD CONSTRAINT sanctionseps58_origine_in_list_chk CHECK (cakephp_validate_in_list((origine)::text, ARRAY['radiepe'::text, 'noninscritpe'::text, 'nonrespectcer'::text, 'nonrespectppae'::text]));
