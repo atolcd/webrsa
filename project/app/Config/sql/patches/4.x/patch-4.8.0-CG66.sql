d0f156c4e39570404fad3777aa87097222cc5efa
MMa <mma@atolcd.com>
2020-09-21 13:05:25 +0200

    Correction des résultats de recherche APRE 66
    Modification de la catégorie de configuration
    
    Issue: #97609
    Change-Id: I6a8460cc4c75c16f9bbfbaa367016e2e00d648f9

--- project/app/Config/sql/patches/4.x/patch-4.8.0-CG66.sql
+++ project/app/Config/sql/patches/4.x/patch-4.8.0-CG66.sql
@@ -7,6 +7,10 @@ BEGIN;
 -- Adaptation du PPAE pour le 66
 UPDATE public.regroupementseps SET sanctionep58 = 'decisionep';
 
+-- Adaptation du module Apres66 pour les recherches
+UPDATE public.configurations SET configurationscategorie_id = configurationscategories.id FROM configurationscategories WHERE configurationscategories.lib_categorie = 'Apres' AND configurations.lib_variable LIKE 'ConfigurableQuery.Apres.search';
+UPDATE public.configurations SET configurationscategorie_id = configurationscategories.id FROM configurationscategories WHERE configurationscategories.lib_categorie = 'Apres' AND configurations.lib_variable LIKE 'ConfigurableQuery.Apres.exportcsv';
+
 -- *****************************************************************************
 COMMIT;
 -- *****************************************************************************
