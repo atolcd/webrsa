a0fdd9288a83d406a92de6f2da1a94d5c1694fe9
pla <pla@atolcd.com>
2021-02-09 12:02:28 +0100

    Ajout de IF NOT EXIST sur l'ajout de colonnes
    Change-Id: I1176f840f723bf3d96a37575b9530774a5a3fd9d

--- project/app/Config/sql/patches/4.x/patch-4.9.0.sql
+++ project/app/Config/sql/patches/4.x/patch-4.9.0.sql
@@ -4,7 +4,7 @@ SET client_encoding = 'UTF8';
 BEGIN;
 -- *****************************************************************************
 -- Ajout de la colonne email pour les structures référentes
-ALTER TABLE public.structuresreferentes ADD COLUMN email varchar(250);
+ALTER TABLE public.structuresreferentes ADD COLUMN IF NOT EXISTS email varchar(250);
 
 
 -- *****************************************************************************
