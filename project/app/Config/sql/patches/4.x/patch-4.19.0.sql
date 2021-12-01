767f6de55942586e8dc9245d641c1240d9d93f6a
csc <csc@atolcd.com>
2021-12-01 14:20:17 +0100

    Ajout de la notion d'actif pour les passages en commissions des rendez-vous (Issue: #128607)
    Change-Id: I955833c2114de77cbccb231234672a57a8085994

--- project/app/Config/sql/patches/4.x/patch-4.19.0.sql
+++ project/app/Config/sql/patches/4.x/patch-4.19.0.sql
@@ -3,6 +3,8 @@ SET client_encoding = 'UTF8';
 -- *****************************************************************************
 BEGIN;
 -- *****************************************************************************
+-- Version du patch en BDD
+INSERT INTO versionpatchsql("version", created) VALUES ('4.19.0', CURRENT_TIMESTAMP);
 
 -- Ajout de la notion d'actif pour les passages en comissions des rendez-vous
 ALTER TABLE public.statutsrdvs_typesrdv ADD IF NOT EXISTS actif bool NOT NULL DEFAULT true;
