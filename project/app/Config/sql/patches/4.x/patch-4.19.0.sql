acdc0bebb97353b389e90ee1d969c370cdde75c0
csc <csc@atolcd.com>
2021-12-01 14:20:17 +0100

    Ajout de la notion d'actif pour les passages en commissions des rendez-vous (Issue: #128607)
    Change-Id: I955833c2114de77cbccb231234672a57a8085994

--- project/app/Config/sql/patches/4.x/patch-4.19.0.sql
+++ project/app/Config/sql/patches/4.x/patch-4.19.0.sql
@@ -3,6 +3,8 @@ SET client_encoding = 'UTF8';
 -- *****************************************************************************
 BEGIN;
 -- *****************************************************************************
+-- Version du patch en BDD
+INSERT INTO versionpatchsql("version", created) VALUES ('4.19.0', CURRENT_TIMESTAMP);
 
 -- Ajout de la notion d'actif pour les actions de rendez-vous
 ALTER TABLE public.statutsrdvs_typesrdv ADD IF NOT EXISTS actif bool NOT NULL DEFAULT true;
@@ -11,12 +13,13 @@ ALTER TABLE public.statutsrdvs_typesrdv ADD IF NOT EXISTS actif bool NOT NULL DE
 ALTER TABLE public.statutsrdvs_typesrdv ALTER COLUMN typecommission TYPE varchar(20) USING typecommission::varchar;
 
 -- Modification des contraintes de la table statutsrdvs_typesrdvs pour permettre l'utilisation du type de comission 'ORIENTATION'
-ALTER TABLE public.statutsrdvs_typesrdv DROP CONSTRAINT statutsrdvs_typesrdv_typecommission_in_list_chk;
+ALTER TABLE public.statutsrdvs_typesrdv DROP CONSTRAINT IF EXISTS statutsrdvs_typesrdv_typecommission_in_list_chk;
 ALTER TABLE public.statutsrdvs_typesrdv ADD CONSTRAINT statutsrdvs_typesrdv_typecommission_in_list_chk CHECK (cakephp_validate_in_list((typecommission)::text, ARRAY['cov'::text, 'ep'::text, 'orientation'::text]));
 
 -- Assouplissement de la contrainte d'enregistrement des actions de rendez-vous
 DROP INDEX IF EXISTS public.statutsrdvs_typesrdv_statutrdv_id_typerdv_id_idx;
 CREATE UNIQUE INDEX IF NOT EXISTS statutsrdvs_typesrdv_statutrdv_id_typerdv_id_typecommission_idx ON public.statutsrdvs_typesrdv (statutrdv_id,typerdv_id,typecommission);
+
 
 -- *****************************************************************************
 COMMIT;
