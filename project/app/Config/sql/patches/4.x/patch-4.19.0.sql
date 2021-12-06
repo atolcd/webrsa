f0ba7f28786834ea6943b670f44294dd33f312cf
csc <csc@atolcd.com>
2021-12-06 10:26:15 +0100

    Création automatique des orientations. Tickets 3.1 à 3.3 (Issue: #128608)
    Change-Id: Ic57eba2b0d8f8e1693daa37e44f33bdb97ae75ea

--- project/app/Config/sql/patches/4.x/patch-4.19.0.sql
+++ project/app/Config/sql/patches/4.x/patch-4.19.0.sql
@@ -3,8 +3,6 @@ SET client_encoding = 'UTF8';
 -- *****************************************************************************
 BEGIN;
 -- *****************************************************************************
--- Version du patch en BDD
-INSERT INTO versionpatchsql("version", created) VALUES ('4.19.0', CURRENT_TIMESTAMP);
 
 -- Ajout de la notion d'actif pour les actions de rendez-vous
 ALTER TABLE public.statutsrdvs_typesrdv ADD IF NOT EXISTS actif bool NOT NULL DEFAULT true;
