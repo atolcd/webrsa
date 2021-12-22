85f24fc89549dd63350a40ad57589280fbf39b63
csc <csc@atolcd.com>
2021-12-22 10:15:06 +0100

    Modification nom du patch pour version 4.19.1
    Change-Id: Ifd782503201f503c238104a35509a0ad7894f86a

--- project/app/Config/sql/patches/4.x/patch-4.19.1.sql
+++ project/app/Config/sql/patches/4.x/patch-4.19.1.sql
@@ -4,6 +4,9 @@ SET client_encoding = 'UTF8';
 BEGIN;
 -- *****************************************************************************
 
+-- Version du patch en BDD
+INSERT INTO versionpatchsql("version", created) VALUES ('4.19.1', CURRENT_TIMESTAMP);
+
 -- Suppression des variables de configuration obsolètes
 DELETE
 FROM public.configurations
