ff642431af42d90ae1131a5f3385be09460221da
csc <csc@atolcd.com>
2022-03-03 11:45:26 +0100

    Réorganisation du menu du plan pauvreté pour le CD58 (Issue: #134672)
    Change-Id: Ifd7ac023a2722c20b4f92bb17ccdb6c11fbdabc7

--- project/app/Config/sql/patches/4.x/patch-4.21.0-CG58.sql
+++ project/app/Config/sql/patches/4.x/patch-4.21.0-CG58.sql
@@ -7,6 +7,16 @@ BEGIN;
 -- Update de la variable de configuration pour l'affichage des referents sectorisation
 UPDATE public.configurations SET value_variable = 'true' WHERE lib_variable LIKE 'PlanPauvrete.Nouveauxentrants.PPAE';
 
+
+-- Update des variables de configuration pour ne pas afficher le rdv 3 en 1 dans le menu du plan pauvreté
+UPDATE public.configurations SET value_variable = 'false' WHERE lib_variable LIKE 'Module.Cohorte.Plan.Pauvrete.Nouveaux.cohorte_infocol_second_rdv_nouveaux';
+UPDATE public.configurations SET value_variable = 'false' WHERE lib_variable LIKE 'Module.Cohorte.Plan.Pauvrete.Nouveaux.cohorte_infocol_imprime_second_rdv_nouveaux';
+UPDATE public.configurations SET value_variable = 'false' WHERE lib_variable LIKE 'Module.Cohorte.Plan.Pauvrete.Nouveaux.cohorte_infocol_venu_nonvenu_second_rdv_nouveaux';
+UPDATE public.configurations SET value_variable = 'false' WHERE lib_variable LIKE 'Module.Cohorte.Plan.Pauvrete.Stock.cohorte_infocol_second_rdv_stock';
+UPDATE public.configurations SET value_variable = 'false' WHERE lib_variable LIKE 'Module.Cohorte.Plan.Pauvrete.Stock.cohorte_infocol_imprime_second_rdv_stock';
+UPDATE public.configurations SET value_variable = 'false' WHERE lib_variable LIKE 'Module.Cohorte.Plan.Pauvrete.Stock.cohorte_infocol_venu_nonvenu_second_rdv_stock';
+
+
 -- *****************************************************************************
 COMMIT;
 -- *****************************************************************************
