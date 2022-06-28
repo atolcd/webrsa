ffb628e9c96bd19c8ed2b47f5d79038e00fefab4
csc <csc@atolcd.com>
2022-06-28 15:22:37 +0200

    Update du critère 7 pour pouvoir modifier le nombre d'enfants à charge (Issue: #147349)
    Change-Id: I1e86c98e5523e48fd76b3d48d70a936e64537cba

--- project/app/Config/sql/patches/4.x/patch-4.23.4-CG93.sql
+++ project/app/Config/sql/patches/4.x/patch-4.23.4-CG93.sql
@@ -7,6 +7,11 @@ BEGIN;
 --Insertion de valeurs dans le champ libelle_initial
 update public.criteresalgorithmeorientation set libelle_initial = libelle;
 
+-- Update du critère 7 pour pouvoir modifier le nombre d'enfants à charge
+UPDATE criteresalgorithmeorientation
+SET libelle = 'L''allocataire est dans un foyer monoparental avec %d enfants ou plus à charge ?', nb_enfants = 1
+WHERE code = 'FOYER_MONOPARENTAL';
+
 -- *****************************************************************************
 COMMIT;
 -- *****************************************************************************
