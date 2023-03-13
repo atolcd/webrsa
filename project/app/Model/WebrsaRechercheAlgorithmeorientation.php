c9ddbfa86875ed155eec2705fd19c375b9a51bda
csc <csc@atolcd.com>
2023-03-13 15:33:12 +0100

    Suppression du distinct sur personne_id dans la liste des nouveaux orientés de l'algorithme d'orientation (Issue: #170184)
    Change-Id: I8ca8598f465446aae83d62f382554940cb7b7395

--- project/app/Model/WebrsaRechercheAlgorithmeorientation.php
+++ project/app/Model/WebrsaRechercheAlgorithmeorientation.php
@@ -97,7 +97,7 @@
 
 				// 1. Ajout des champs supplémentaires
 				$query['fields'] = array_merge(
-					['DISTINCT ON ("Personne"."id") "Personne"."id" as "Personne__id"'],
+					['"Personne"."id" as "Personne__id"'],
 					$query['fields'],
 					ConfigurableQueryFields::getModelsFields(
 						array(
